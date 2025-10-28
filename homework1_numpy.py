0884aba9c9aea9fcf9bfea0dd77aeab2b8bceb5c
cacsibal <cacsibal@gmail.com>
2025-10-28 11:19:20 -0400

    done?

--- homework1_numpy.py
+++ homework1_numpy.py
@@ -16,25 +16,33 @@ def problem5 (A, c, d):
     return np.mean(A[(A >= c) & (A <= d)])
 
 def problem6 (A, k):
-    return ...
+    eigenvalues, eigenvectors = np.linalg.eig(A)
+    top_k = np.argsort(np.abs(eigenvalues))[::-1][:k]
+    return eigenvectors[:, top_k]
 
 def problem7 (A, x):
     return np.linalg.solve(A, x)
 
 def problem8 (x, k):
-    return ...
+    x = np.atleast_2d(x).reshape(-1, 1)
+    return np.repeat(x, k, axis=1)
 
 def problem9 (A):
-    return ...
+    return A[np.random.permutation(A.shape[0])]
 
 def problem10 (A):
-    return ...
+    return np.mean(A, axis=1)
 
 def problem11 (n, k):
-    return ...
+    A = np.random.randint(0, k + 1, size=n)
+    A[A % 2 == 0] = -1
+    return A
 
 def problem12 (A, b):
-    return ...
+    b = np.atleast_2d(b).reshape(-1, 1)
+    B = np.repeat(b, A.shape[1], axis=1)
+    return A + B
 
 def problem13 (A):
-    return ...
+    n, m, _ = A.shape
+    return A.reshape(n, m * m).T
--- test.py
+++ test.py
@@ -4,9 +4,17 @@ import homework1_numpy as hw
 A = np.array([[1, 2], [3, 4]])
 B = np.array([[5, 6], [7, 8]])
 C = np.array([[9, 10], [11, 12]])
+x = np.array([1, 2])
+
+D = np.array([A, B, C])
 
 # print(hw.problem1(A, B, C))
 # print(hw.problem2(A))
 # print(hw.problem3(A))
 # print(hw.problem4(A, 0))
 # print(hw.problem5(A, 2, 3))
+# print(hw.problem10(A))
+# print(hw.problem8(x, 3))
+# print(hw.problem11(10, 10))
+# print(hw.problem12(A, x))
+print(hw.problem13(D))
